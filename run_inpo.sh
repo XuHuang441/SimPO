49e0b886b3bdbabd962d0c55cca4f8d4aa313803
huangxu <xhuang441@qq.com>
2025-08-06 10:32:31 +0800

    add batch size to decode.py to avoid OOM

--- run_inpo.sh
+++ run_inpo.sh
@@ -42,13 +42,15 @@ echo "Starting iteration 2"
 # on policy data gen
 echo "iter2: Starting on policy data gen"
 
-for SEED in 13 21 42 79 100
+#for SEED in 13 21 42 79 100
+for SEED in 21 42 79 100
   do
      echo "Running decode with seed $SEED..."
      stdbuf -oL -eL /home/hubing/miniconda3/envs/inpo/bin/python -u -m on_policy_data_gen.decode \
      --data_dir "/home/hubing/SimPO/data/gemma2_ufb_part2.jsonl" \
      --seed "$SEED" \
      --output_dir "/home/hubing/SimPO/datasets/gemma2_ultrafeedback/inpo_iter2" \
+     --batch_size 16 \
      --num_gpu 8 # Tensor Parallelism
   done
 
