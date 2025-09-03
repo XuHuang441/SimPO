e95532a66cf9dbdc682d83bfb184da49b40f209c
huangxu <xhuang441@qq.com>
2025-09-03 12:32:00 +0800

    run tdpo iter2 off policy lr = 2e-7; add code for eval

--- run_tdpo_off_policy.sh
+++ run_tdpo_off_policy.sh
@@ -9,32 +9,33 @@ export PYTHONPATH=$(pwd)
 history_paths=()
 
 # divide dataset into 3 subsets with 20000 rows each.
-#conda run -n inpo python -m inpo_scripts.split_dataset
+conda run -n inpo python -m inpo_scripts.split_dataset
 
 # ------------------------iter1------------------------
 history_args=""
 
 #  precompute # --config_file ./accelerate_configs/zero2.yaml
-#/home/hubing_google_com/miniconda3/envs/sim/bin/accelerate launch --num_processes=8 -m inpo_scripts.precompute_simpo_style \
-#     --run_name "inpo_iter1" \
-#     --train_dir "/home/hubing_google_com/SimPO/data/gemma2_ufb_part1.jsonl" \
-#     --output_dir "data/inpo_iter1/pref" \
-#     --ref_model google/gemma-2-9b-it --last_model google/gemma-2-9b-it \
-#     --loss_type inpo --lr_scheduler_type cosine \
-#     $history_args \
-#     --sanity_check False
+/home/hubing_google_com/miniconda3/envs/sim/bin/accelerate launch --num_processes=8 -m inpo_scripts.precompute_simpo_style \
+     --run_name "inpo_iter1" \
+     --train_dir "/home/hubing_google_com/SimPO/data/gemma2_ufb_part1.jsonl" \
+     --output_dir "data/inpo_iter1/pref" \
+     --model_name_or_path "google/gemma-2-9b-it" \
+     --ref_model google/gemma-2-9b-it --last_model google/gemma-2-9b-it \
+     --loss_type inpo --lr_scheduler_type cosine \
+     $history_args \
+     --sanity_check False
 
 # train
-#echo "iter1: start training"
-#
-#ACCELERATE_LOG_LEVEL=info /home/hubing_google_com/miniconda3/envs/sim/bin/accelerate launch \
-#    --config_file accelerate_configs/deepspeed_zero3.yaml \
-#    -m inpo_scripts.run_inpo \
-#    training_configs/gemma-2-9b-it-inpo-iter1.yaml \
+echo "iter1: start training"
+
+ACCELERATE_LOG_LEVEL=info /home/hubing_google_com/miniconda3/envs/sim/bin/accelerate launch \
+    --config_file accelerate_configs/deepspeed_zero3.yaml \
+    -m inpo_scripts.run_inpo \
+    training_configs/gemma-2-9b-it-inpo-iter1.yaml \
 
 history_paths+=("/home/hubing_google_com/SimPO/outputs/gemma-2-9b-it_inpo_stage_1/")
 
-#echo "Completed iteration 1"
+echo "Completed iteration 1"
 
 # ------------------------iter2------------------------
 echo "Starting iteration 2"
